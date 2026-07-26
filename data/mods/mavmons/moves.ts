970afb207503634eb5954e31c18783692db9de2e
LillieCharms <lilliecharms@gmail.com>
2026-07-26 03:35:46 -0400

    Update moves.ts

--- data/mods/mavmons/moves.ts
+++ data/mods/mavmons/moves.ts
@@ -1642,7 +1642,7 @@ export const Moves: {[k: string]: ModdedMoveData} = {
     accuracy: 100,
     basePower: 50,
 	
-		const: cheezrage = pokemon.volatiles.smashrage;
+		const cheezrage = pokemon.volatiles.smashrage;
 
 		if (cheezrage) {
 			return 350;
@@ -1700,7 +1700,7 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		type: "Fighting",
 		pp: 10,
 		contact: true,
-	}
+	},
 	pretzeldog: {
 		num: -48,
 		accuracy: true,
