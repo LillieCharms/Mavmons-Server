a942bab32da95f5795aecda183e117737296c2bd
Dia Dia <sygnasuitdiantha@gmail.com>
2026-09-11 23:28:20 -0400

    lewyn bugfix

--- data/mods/mavmons/pokedex.ts
+++ data/mods/mavmons/pokedex.ts
@@ -95,7 +95,7 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		types: ["Water", "Steel"],
 		gender: "M",
 		baseStats: {hp: 100, atk: 90, def: 110, spa: 165, spd: 110, spe: 92},
-		abilities: {0: "Laser Pressure", 1: "Super Quick Jump", H: "Sniper"},
+		abilities: {0: "Laser Pressure", 1: "Quick Super Jump", H: "Sniper"},
 		weightkg: 75,
 	},
 	randeez: {
